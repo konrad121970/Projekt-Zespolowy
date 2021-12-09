f1efb3587eee6679450497d6480b522092da9998
Bartosz <nieznajomy027@gmail.com>
2021-12-09 21:59:17 +0100

    Updated scripts

--- scripts/sql_query/insert_values.sql
+++ scripts/sql_query/insert_values.sql
@@ -6,3 +6,11 @@ INSERT INTO [dbo].[Account] ([ID], [Username], [Password]) VALUES (1, N'pudzian0
 INSERT INTO [dbo].[Account] ([ID], [Username], [Password]) VALUES (2, N'karmelek17', N'AQAAAAEAACcQAAAAEHFMN1Lwea+APi4wHk2Tu4yGy7WSk4Gyy6uAdmS/2KwOZkVnZ74D+geZpGDg8QQvmg==')
 SET IDENTITY_INSERT [dbo].[Account] OFF
 
+-- Conversation
+SET IDENTITY_INSERT [dbo].[Conversation] ON
+INSERT INTO [dbo].[Conversation] ([ID], [Name]) VALUES (1, N'...')
+SET IDENTITY_INSERT [dbo].[Conversation] OFF
+
+-- Account_Conversation
+INSERT INTO [dbo].[Account_Conversation] ([Member_ID], [Conversation_ID]) VALUES (1, 1)
+INSERT INTO [dbo].[Account_Conversation] ([Member_ID], [Conversation_ID]) VALUES (2, 1)
