bda5ba8b47e1c4d62b7c2670d70ff7c189f86c1c
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2022-03-28 14:00:46 +0200

    Widok serwera + lekka poprawa powiadomień

--- apps/ClientApp/MVVM/ViewModel/MainViewModel.cs
+++ apps/ClientApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-
+using System.Windows;
 using ClientApp.Core;
 using ClientApp.MVVM.Model;
 
@@ -10,25 +10,44 @@ namespace ClientApp.MVVM.ViewModel {
 
     class MainViewModel : ObservableObject {
         public MainViewModel() {
+
             FriendList = new ObservableCollection<User>();
+            //dodaje
+            FriendListVisibility = Visibility.Visible;
+            ServerListVisibility = Visibility.Hidden;
+            //
 
             CurrentUser = new User();
             CurrentUser.Id = Client.Data.UserID;
+            HomeVM = new HomeViewModel()
+            {
+                FriendList = FriendList
+            };
 
             HomeViewCommand = new RelayCommand(obj => {
-                HomeVM = new HomeViewModel() {
-                    FriendList = FriendList
-                };
-
+              
                 CurrentView = HomeVM;
             });
 
             ChatViewCommand = new RelayCommand(obj => {
                 var user = obj as User;
-
+                
                 ChatVM = new ChatViewModel(user);
+
                 CurrentView = ChatVM;
             });
+            //to dodałem
+            FriendListViewCommand = new RelayCommand(obj => {
+                MakeFriendListVisible();
+            });
+
+            ServerListViewCommand = new RelayCommand(obj => {
+                MakeServerListVisible();
+                ServerVM = new ServerViewModel();
+                CurrentView = ServerVM;
+            });
+
+            //
         }
 
         public void SetFriendList(List<string> friends) {
@@ -39,20 +58,36 @@ namespace ClientApp.MVVM.ViewModel {
             }
         }
 
+        public void MakeFriendListVisible() 
+        {
+                ServerListVisibility = Visibility.Hidden;
+                FriendListVisibility = Visibility.Visible;
+        }
+
+        public void MakeServerListVisible()
+        {
+            ServerListVisibility = Visibility.Visible;
+            FriendListVisibility = Visibility.Hidden;
+        }
+
         // Commands
         public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand FriendListViewCommand { get; set; }
+        public RelayCommand ServerListViewCommand { get; set; }
         public RelayCommand ChatViewCommand { get; set; }
 
         // VM's
         public HomeViewModel HomeVM { get; set; }
         public ChatViewModel ChatVM { get; set; }
+        public ServerViewModel ServerVM { get; set; }
 
         // User data
         public User CurrentUser { get; set; }
         public ObservableCollection<User> FriendList { get; set; }
- 
+
         // Control data
-        public object CurrentView {
+        public object CurrentView 
+        {
             get {
                 return _CurrentView;
             }
@@ -62,6 +97,34 @@ namespace ClientApp.MVVM.ViewModel {
             }
         }
         private object _CurrentView;
+
+        public Visibility FriendListVisibility
+        {
+            get
+            {
+                return _FriendListVisibility;
+            }
+            set
+            {
+                _FriendListVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+        private Visibility _FriendListVisibility;
+
+        public Visibility ServerListVisibility
+        {
+            get
+            {
+                return _ServerListVisibility;
+            }
+            set
+            {
+                _ServerListVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+        private Visibility _ServerListVisibility;
     }
 
 }
